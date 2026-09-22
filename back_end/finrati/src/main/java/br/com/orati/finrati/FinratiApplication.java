53e16bd2142c4d328d124609a0b19b6ccfe9b573
Raphael Orati <misterbroderkill@gmail.com>
2026-09-22 06:53:04 -0300

    feat: inicialização de projeto Spring, configuração de flyway e ajustes inclusão de .env e config de banco de dados

---
+++ back_end/finrati/src/main/java/br/com/orati/finrati/FinratiApplication.java
@@ -0,0 +1,13 @@
+package br.com.orati.finrati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinratiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinratiApplication.class, args);
+	}
+
+}
---
+++ back_end/finrati/src/test/java/br/com/orati/finrati/FinratiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orati.finrati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinratiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
